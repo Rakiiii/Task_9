2423e115618684c209200e6187b87e94072cad16
Rakiiii <sudaru2@gmail.com>
2017-10-06 09:56:19 +0300

    refactored

--- Include_and_constants.h
+++ Include_and_constants.h
@@ -4,7 +4,7 @@
 
 #ifndef UNTITLED_INCLUDE_AND_CONSTANTS_H
 #define UNTITLED_INCLUDE_AND_CONSTANTS_H
-#include <stdio.h>
+#include <stdio.h> // подключаем все необходимые библиотеки
 #include <stdlib.h>
 #include <conio.h>
 #include <locale.h>
@@ -14,103 +14,103 @@
 
 int In() //ввод с защитой от юзера
 {
-    int RightNumber = 0;
-    while(scanf("%d" , &RightNumber) != 1)
+    int RightNumber = 0 ;
+    while(scanf( "%d" , &RightNumber ) != 1 )
     {
-        printf("Uncorect datatype, print once more_");
-        while(getchar() != '\n');
+        printf( "Uncorect datatype, print once more_" ) ;
+        while( getchar() != '\n' ) ;
     }
-    return RightNumber;
+    return RightNumber ;
 }
 
 
-int BinaryNumbers(int n , int l ) // преобразование 10 го числа к двоичному
+int BinaryNumbers( int n , int l ) // преобразование 10 го числа к двоичному
 {
-    n = abs(n);
-    for(int i = l ; i >= 0 ; i--)
+    n = abs( n ) ;
+    for( int i = l ; i >= 0 ; i-- )
     {
-        printf("%d" , (n >> i) & 1);
+        printf( "%d" , ( n >> i ) & 1) ;
     }
-    printf("   ");
-    return 0;
+    printf("   ") ;
+    return 0 ;
 }
 
 
-int Exponentiation(int  Number, int Exponent ) // возведение в степень
+int Exponentiation( int  Number, int Exponent ) // возведение в степень
 {
-    int Result = Number;
-   for(int i = 0 ; i < Exponent ; i++ )
+    int Result = Number ;
+   for( int i = 0 ; i < Exponent ; i++ )
    {
        Result = Result * Number ;
    }
-    return Result;
+    return Result ;
 }
 
-int BinaryLength(int Number)// подсчет количества разрядов в двоичной форме 10 го числа
+int BinaryLength( int Number )// подсчет количества разрядов в двоичной форме 10 го числа
 {
     int Length = 0 ;
-    Number = abs(Number);
+    Number = abs( Number ) ;
     while( Number > 0 )
     {
-        Number = Number >> 1;
-        Length ++;
+        Number = Number >> 1 ;
+        Length ++ ;
     }
-    return Length - 1;
+    return Length - 1 ;
 }
 
-int StringToNumber(char *String)//преобразование строки в число
+int StringToNumber( char *String )//преобразование строки в число
 {
-    int Number = 0;  //конечное число
-    int i = 0; //счетчик
-    int Sign = 0; //знак
-    if (String[i] == '-') //определяем знак
+    int Number = 0 ;  //конечное число
+    int i = 0 ; //счетчик
+    int Sign = 0 ; //знак
+    if ( String[i] == '-' ) //определяем знак
     {
-        Sign = 1;
-        i++;
+        Sign = 1 ;
+        i ++ ;
     }
-    while (String[i] >= 0x30 && String[i] <= 0x39) //преобразовываем на основе ASKII
+    while ( String[ i ] >= 0x30 && String[ i ] <= 0x39 ) //преобразовываем на основе ASKII
     {
-        Number = Number + (String[i] & 0x0F);
-        Number = Number * 10;
-        i++;
+        Number = Number + ( String[ i ] & 0x0F );
+        Number = Number * 10 ;
+        i ++ ;
     }
-    Number = Number / 10;
-    if (Sign == 1)
-        Number = -Number; //разбираемся со знаком
-    return Number;
+    Number = Number / 10 ;
+    if ( Sign == 1 )
+        Number = -Number ; //разбираемся со знаком
+    return Number ;
 }
 
-char* NumberToString(int Number) // преобразование числа в строку
+char* NumberToString( int Number ) // преобразование числа в строку
 {
-    char* String = (char*)malloc( 255 * sizeof( char ) ) , t , *Temp;// делаем Два указателя из одного сразу массив, и переменную для обмена
-    int i = 0 , k = Number ,  Sign = 0; // считчик, копия числа , и опрделитель знака
+    char* String = ( char* )malloc( 255 * sizeof( char ) ) , t , *Temp;// делаем Два указателя из одного сразу массив, и переменную для обмена
+    int i = 0 , k = Number ,  Sign = 0 ; // считчик, копия числа , и опрделитель знака
     if( k < 0 ) //определяем знак
     {
-        Sign = 1;
-        k = -k;
+        Sign = 1 ;
+        k = -k ;
     }
     do {
         t = k % 10 ; //разбираем копию числа
         k = k / 10 ;
         String[ i ] = t | 0x30 ; //преобразовываем на основе ASKII
         i ++ ;
-    }while( k > 0 );
+    }while( k > 0 ) ;
     if ( Sign == 1 )//разбираемяся со знаком
     {
-        String[ i ] = '-';
-        i ++;
+        String[ i ] = '-' ;
+        i ++ ;
     }
-    Temp = (char*)malloc( i * sizeof( char ) );// т.к. строка перевернута создаем еще одну для удобства переворачивания
-    k = 0;
-    i --;
-    while(i >= 0)// переыорачиваем строку
+    Temp = ( char* )malloc( i * sizeof( char ) );// т.к. строка перевернута создаем еще одну для удобства переворачивания
+    k = 0 ;
+    i -- ;
+    while( i >= 0 )// переыорачиваем строку
     {
-        Temp[ k ] = String [ i ];
-        i ++;
-        k --;
+        Temp[ k ] = String [ i ] ;
+        i ++ ;
+        k -- ;
     }
-    Temp[ k ] = '\0';
-    return *Temp;
+    Temp[ k ] = '\0' ;
+    return Temp ;
 }
 
 
--- main.c
+++ main.c
@@ -1,39 +1,39 @@
 #include <windef.h>
 #include "Include_and_constants.h"
 
-int WithPersonalLib()
+int WithPersonalLib() //превращаем число в строку и обратно с помощью своей библиотеки
 {
     while (TRUE)
     {
-        char* String = (char*)malloc( 255 * sizeof( char ) );
-        int Number;
-        system("cls");
-        printf("1.Translate string to number\n2.Translate number to string\n3.Back to main menu");
-        char Choose = getch();
-        switch (Choose)
+        char* String = ( char* )malloc( 255 * sizeof( char ) ) ; //выделяем память под нашу строку
+        int Number ; //и для числа
+        system( "cls" )  ;
+        printf( "1.Translate string to number\n2.Translate number to string\n3.Back to main menu" ) ;
+        char Choose = getch() ; //подраздел меню
+        switch ( Choose )
         {
             case '1' :
-                system("cls");
-                printf("Add string_\n");
-                scanf("%s" , String);
-                Number = StringToNumber( String );
-                printf("Number is %d" , Number);
-                printf("Press any button to restart the programme_");
-                getch();
-                break;
+                system( "cls" ) ;
+                printf( "Add string_\n" ) ;
+                scanf( "%s" , String ) ; //читаем строку
+                Number = StringToNumber( String ) ; //вызываем функцию перевода строки в чиссло описанную в includes and constants
+                printf( "Number is %d" , Number ) ; //выводим число
+                printf( "Press any button to restart the programme_" ) ;
+                getch() ;
+                break ;
             case '2' :
-                system("cls");
-                printf("Add number_\n");
-                scanf("%d" , &Number);
-                String = NumberToString( Number );
-                printf("String is %s" , String);
-                printf("Press any button to restart the programme_");
-                getch();
-                break;
+                system( "cls" ) ;
+                printf( "Add number_\n" ) ;
+                scanf( "%d" , &Number ) ; //читаем число
+                String = NumberToString( Number ) ; //призываем функцию перевода числа в строку описанную в includes and constants
+                printf( "String is %s" , String ) ; //воводим строку
+                printf( "Press any button to restart the programme_" ) ;
+                getch() ;
+                break ;
             case '3' :
-                return 0;
+                return 0 ;
             default:
-                break;
+                break ;
         }
     }
 }
@@ -41,38 +41,38 @@ int WithPersonalLib()
 
 int WithStdlib()
 {
-    while (TRUE)
+    while (TRUE) //подраздел меню
     {
-        char *String = (char*)malloc( 255 * sizeof( char ) );
-        int Number;
-        char* end_ptr;
-        system("cls");
-        printf("1.Translate string to number\n2.Translate number to string\n3.Back to main menu");
-        char Choose = getch();
-        switch (Choose)
+        char *String = ( char* )malloc( 255 * sizeof( char ) ) ; //выделяем память под строку
+        int Number ; //и под число
+        char* end_ptr ; //и указатель для функции strtol для ошибок
+        system( "cls" ) ;
+        printf( "1.Translate string to number\n2.Translate number to string\n3.Back to main menu" ) ;
+        char Choose = getch() ;
+        switch ( Choose )
         {
             case '1' :
-                system("cls");
-                printf("Add string_\n");
-                scanf("%s" , String);
-                Number = strtol( String, &end_ptr , 10);
-                printf("Number is %d\n" , Number);
-                printf("\nPress any button to restart the programme_");
-                getch();
-                break;
+                system( "cls" ) ;
+                printf( "Add string_\n" ) ;
+                scanf( "%s" , String ) ; //читаем строку
+                Number = strtol( String, &end_ptr , 10 ) ; //превращяем строку в числос с помощью стандартной функции
+                printf( "Number is %d\n" , Number ) ; //выводим число
+                printf( "\nPress any button to restart the programme_" ) ;
+                getch() ;
+                break ;
             case '2' :
-                system("cls");
-                printf("Add number_\n");
-                scanf("%d" , &Number);
-                itoa(Number , String ,10 );//sprintf(*String ,"%d" , Number);
-                printf("Your string is :%s\n" , String);
-                printf("\nPress any button to restart the programme_");
-                getch();
-                break;
+                system( "cls" ) ;
+                printf( "Add number_\n" ) ;
+                scanf( "%d" , &Number ) ; //читаем число
+                itoa( Number , String ,10 ) ;//sprintf(*String ,"%d" , Number); два варианта функций,используемая на данный момент лучше,переводит число в строку
+                printf( "Your string is :%s\n" , String ) ;
+                printf( "\nPress any button to restart the programme_" ) ;
+                getch() ;
+                break ;
             case '3' :
-                return 0;
+                return 0 ;
             default:
-                break;
+                break ;
         }
     }
 }
@@ -81,27 +81,27 @@ int WithStdlib()
 
 int main()
 {
-    while (TRUE)
+    while (TRUE) //меню
     {
-        system("cls");
-        printf("1.Make translation by stdlib\n2.Make translation by my personal lib\n3.Exit");
-        char Choose = getch();
-        switch (Choose)
+        system( "cls" ) ;
+        printf( "1.Make translation by stdlib\n2.Make translation by my personal lib\n3.Exit" ) ;
+        char Choose = getch() ;
+        switch ( Choose )
         {
             case '1' :
-                WithStdlib();
-                printf("\nPress any button to restart the programme_");
-                getch();
-                break;
+                WithStdlib() ;
+                printf( "\nPress any button to restart the programme_" ) ;
+                getch() ;
+                break ;
             case '2' :
-                WithPersonalLib();
-                printf("\nPress any button to restart the programme_");
-                getch();
-                break;
+                WithPersonalLib() ;
+                printf( "\nPress any button to restart the programme_" ) ;
+                getch() ;
+                break ;
             case '3' :
-                return 0;
+                return 0 ;
             default:
-                break;
+                break ;
 
         }
     }
